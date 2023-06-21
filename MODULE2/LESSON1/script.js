669c5a0699ebfd3837ebd25e18d001604075fbe9
DTerenz <84399369+DTerenz@users.noreply.github.com>
2023-06-21 18:42:43 +0300

    Add files via upload

---
+++ MODULE2/LESSON1/script.js
@@ -0,0 +1,35 @@
+//               TASK 1             //
+
+
+
+// let city = "kyiv";
+// city = "lviv"
+// let address = city;
+// alert(address);
+
+
+
+
+//               TASK 2             //
+
+
+// let n = 5;
+// result = Math.pow(n, 3);
+// console.log(result)
+
+
+
+
+
+//               TASK 3             //
+
+
+
+// let pen = 4;
+// let penCost = 5.25;
+// let pencil = 6;
+// let pencilCost = 3.50;
+// let totalPrice;
+// totalPrice = (pen * penCost) + (pencil * pencilCost);
+// result = totalPrice;
+// document.write(result);
