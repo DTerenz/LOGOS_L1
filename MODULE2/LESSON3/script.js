32495e81e2550f9dc9e9abbe4a7b42f5f1b15026
DTerenz <84399369+DTerenz@users.noreply.github.com>
2023-07-07 20:01:14 +0300

    Add files via upload

---
+++ MODULE2/LESSON3/script.js
@@ -0,0 +1,46 @@
+//                        TASK 1                                  //
+
+// for(let i = 1000; i <= 9999; i +=3){
+//   console.log(i)
+// }
+// let i = 1000;
+// while(i<=9999){
+//   console.log(i)
+//   i+=3
+// }
+
+//                        TASK 2                                  //
+
+// for (let a = 1, b = 1; a <= 55; a++, b += 5) {
+//   console.log(b);
+// }
+// let a = 1;
+// let b = 1;
+// while(a<=55){
+//   console.log(b)
+//   a++;
+//   b+=5
+// }
+
+
+//                        TASK 3                                  //
+// for(let i = 90; i >= 0; i -=5){
+//   console.log(i)
+// }
+// let i = 90;
+// while(i>=0){
+//   console.log(i)
+//   i-=5
+// }
+
+//                        TASK 4                                  //
+// for (let a = 1, b = 2; a <= 20; a++, b += 2) {
+//   console.log(b);
+// }
+// let a = 1;
+// let b = 2;
+// while(a<=20){
+//   console.log(b)
+//   a++;
+//   b+=2
+// }
